8704b3dbd2ddea032e537cac9a80a05195c6f235
John Fereira <jaf30@cornell.edu>
2018-11-13 10:33:06 -0500

    change log level to warning

--- run-jobqueue.sh
+++ run-jobqueue.sh
@@ -2,7 +2,7 @@
 rbenv local 2.5.1
 
 echo Starting Redis
-redis-server /usr/local/etc/redis.conf > redis.log 2>&1 &
+redis-server /usr/local/etc/redis.conf --loglevel warning > redis.log 2>&1 &
 echo Start Sidekiq
 bundle exec sidekiq > sidekiq.log 2>&1 &
 
