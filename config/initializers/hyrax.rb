2004798a6cd921129239ad9e5b279b07c663f37a
John Fereira <jaf30@cornell.edu>
2019-02-06 11:20:22 -0500

    add dataset

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -10,6 +10,8 @@ Hyrax.config do |config|
 #  config.register_curation_concern :"namespaced_works/nested_work"
   # Injected via `rails g hyrax:work Image`
   config.register_curation_concern :image
+  # Injected via `rails g hyrax:work Dataset`
+  config.register_curation_concern :dataset
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
